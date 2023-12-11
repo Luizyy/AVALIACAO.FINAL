7b0d6771d084653c23b19a83ba990f2fcdee4ea6
Luizyy <analuizynascimentodasilva@gmail.com>
2023-12-11 12:04:53 -0300

    AVALIACAO.FINAL.

--- positivosmedia06.py
+++ positivosmedia06.py
@@ -1,14 +1,11 @@
-nuM = [] #criada uma lista para adicionarmos o mumero
-pstv=0
-
-for a in range(5):#repeticao range
-    n = float(input(f'Digite o {a+1}° número: ')) #receber a variavel
-#usamos if para aplicarmos a condicao de 'se o numero colocado é maior que 0"
-    if n>0:
-        pstv = pstv+1
-        nuM.append(n)
-#entao se sim ele executa o bloco entrando na lista e na variavel pstv
-soma = sum(nuM) #e soma os numeros dentro da lista
-print(f"{pstv} valores positivos")#mostra ao usuario seus valores positivos
-#e a media calculada
-print(f"{soma/5}")
+print("POSITIVOS MÉDIA:")
+intero= []   #usar lista para armazenar numeros futuros
+soma = 0   #e o contador para soma os numeros positivos
+for i in range(6): #usar for e range para repeticao
+    num= float(input("digite o número:"))
+    if num>0:                           #usar if para os numeros se enquadrarem nas condicoes necessarias
+        intero.append(num)  # usar append para substituir
+    soma= sum(intero) # somar
+quantidade=len(intero) #contar quantos sao
+media= soma/quantidade # calculo da media
+print(f"{quantidade} valores positivos.\n{media}") # mostrar ao usuario sua resposta
